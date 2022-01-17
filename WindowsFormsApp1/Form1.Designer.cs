e712b41685f4238c80a999b029aeffe4c9e95669
AliRock86 <alicallofduty86@gmail.com>
2022-01-17 12:50:28 +0300

    print_cheque_last_changes

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace WindowsFormsApp1
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp1
 {
     partial class Form1
     {
@@ -28,6 +30,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.printDocument1 = new System.Drawing.Printing.PrintDocument();
             this.printDialog1 = new System.Windows.Forms.PrintDialog();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -37,9 +40,11 @@
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button12 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -65,109 +70,131 @@
             // textBox8
             // 
             this.textBox8.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox8.Location = new System.Drawing.Point(716, 347);
-            this.textBox8.Multiline = true;
+            this.textBox8.Location = new System.Drawing.Point(716, 356);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(109, 47);
-            this.textBox8.TabIndex = 17;
+            this.textBox8.ShortcutsEnabled = false;
+            this.textBox8.Size = new System.Drawing.Size(109, 32);
+            this.textBox8.TabIndex = 8;
             // 
             // textBox7
             // 
             this.textBox7.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox7.Location = new System.Drawing.Point(404, 347);
-            this.textBox7.Multiline = true;
+            this.textBox7.Location = new System.Drawing.Point(404, 356);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(306, 47);
-            this.textBox7.TabIndex = 16;
+            this.textBox7.Size = new System.Drawing.Size(306, 32);
+            this.textBox7.TabIndex = 7;
             // 
             // textBox6
             // 
             this.textBox6.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox6.Location = new System.Drawing.Point(196, 271);
-            this.textBox6.Multiline = true;
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(1009, 34);
-            this.textBox6.TabIndex = 15;
+            this.textBox6.Size = new System.Drawing.Size(1009, 32);
+            this.textBox6.TabIndex = 6;
             this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // textBox5
             // 
             this.textBox5.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox5.Location = new System.Drawing.Point(248, 205);
-            this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(971, 35);
-            this.textBox5.TabIndex = 14;
+            this.textBox5.Size = new System.Drawing.Size(971, 32);
+            this.textBox5.TabIndex = 5;
             this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox4.Location = new System.Drawing.Point(503, 135);
-            this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(143, 36);
-            this.textBox4.TabIndex = 13;
+            this.textBox4.Size = new System.Drawing.Size(143, 32);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox3.Location = new System.Drawing.Point(237, 85);
-            this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(391, 34);
-            this.textBox3.TabIndex = 12;
+            this.textBox3.Size = new System.Drawing.Size(391, 32);
+            this.textBox3.TabIndex = 3;
             this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
-            // textBox2
+            // button11
             // 
-            this.textBox2.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(140, 44);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(164, 35);
-            this.textBox2.TabIndex = 11;
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(1119, 392);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(154, 48);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Print";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.button11.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(1119, 392);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(154, 48);
+            this.button11.TabIndex = 9;
+            this.button11.Text = "Print";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(471, 44);
-            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.textBox1.Size = new System.Drawing.Size(215, 35);
-            this.textBox1.TabIndex = 9;
+            this.textBox1.Size = new System.Drawing.Size(215, 32);
+            this.textBox1.TabIndex = 1;
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(1075, 456);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 22);
+            this.label1.TabIndex = 18;
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(938, 392);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(154, 48);
+            this.button12.TabIndex = 10;
+            this.button12.Text = "Clear";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy-MM-dd";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(140, 49);
+            this.dateTimePicker1.MinDate = new System.DateTime(2022, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(174, 27);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.Value = new System.DateTime(2022, 1, 1, 0, 0, 0, 0);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1306, 452);
+            this.ClientSize = new System.Drawing.Size(1306, 493);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox8);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button11);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Cheque Printer";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -185,9 +212,11 @@
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button11;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,41 +16,47 @@ namespace WindowsFormsApp1
 
         public Form1()
         {
+
             InitializeComponent();
         }
 
-        Bitmap memoryImage;
-
-        private void CaptureScreen()
+        private Boolean CheckTextBox()
         {
-            Graphics myGraphics = this.CreateGraphics();
-            Size s = this.Size;
-            memoryImage = new Bitmap(s.Width, s.Height, myGraphics);
-            Graphics memoryGraphics = Graphics.FromImage(memoryImage);
-            memoryGraphics.CopyFromScreen(this.Location.X, this.Location.Y, 0, 0, s);
+            var res = true;
+            if (String.IsNullOrEmpty(textBox1.Text) && String.IsNullOrEmpty(textBox3.Text) && String.IsNullOrEmpty(textBox4.Text))
+            {  
+                if (String.IsNullOrEmpty(textBox5.Text) && String.IsNullOrEmpty(textBox6.Text) && String.IsNullOrEmpty(textBox7.Text) && String.IsNullOrEmpty(textBox8.Text))
+                {
+                    res = false;
+                }
+                res = false;
+            }
+            return res;
         }
 
         private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
         {
-           // e.Graphics.DrawImage(Properties.Resources.h_001, 470, 242 , 710, 260);
+            dateTimePicker1.CustomFormat = "yyyy-MM-dd";
+            // e.Graphics.DrawImage(Properties.Resources.h_001, 470, 242 , 710, 260);
             Font f = new Font("Arial" , 12 , FontStyle.Regular);
-             e.Graphics.DrawString(textBox2.Text, f, Brushes.Black, 526, 255);
+             e.Graphics.DrawString(dateTimePicker1.Text, f, Brushes.Black, 526, 255);
             e.Graphics.DrawString(textBox1.Text, f, Brushes.Black, 706, 255);
              e.Graphics.DrawString(textBox3.Text, f, Brushes.Black, 615, 276);
             e.Graphics.DrawString(textBox4.Text, f, Brushes.Black, 725, 307);
-            e.Graphics.DrawString(textBox5.Text, f, Brushes.Black, 810, 348);
-            e.Graphics.DrawString(textBox6.Text, f, Brushes.Black, 810, 384);
+            e.Graphics.DrawString(textBox5.Text, f, Brushes.Black, 810, 338);
+            e.Graphics.DrawString(textBox6.Text, f, Brushes.Black, 810, 374);
             e.Graphics.DrawString(textBox7.Text, f, Brushes.Black, 700, 433);
             e.Graphics.DrawString(textBox8.Text, f, Brushes.Black, 838, 433);
         }
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            /* CaptureScreen();
-             if (printDialog1.ShowDialog() == DialogResult.OK)
-             {
-                 printDocument1.Print();
-             }*/
+            if (!CheckTextBox())
+            {
+                label1.Text = "أملاء البيانات الغير مكتملة";
+                return;
+            }
+            label1.Text = "";
             PaperSize paperSize = new PaperSize("test", 1123, 370);
              paperSize.RawKind = (int)PaperKind.Custom;
              printDocument1.DefaultPageSettings.PaperSize = paperSize;
@@ -60,6 +66,28 @@ namespace WindowsFormsApp1
              {
                  printDocument1.Print();
              }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+           // textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox8.Text = "";
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
